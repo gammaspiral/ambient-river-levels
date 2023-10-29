fe1adb3d76fe96ec1fb04335cf47e58c210b80ea
gammaspiral <146213717+gammaspiral@users.noreply.github.com>
2023-10-29 12:46:52 +0000

    Update music.js

--- music.js
+++ music.js
@@ -37,6 +37,7 @@ function mapDataToPitch(dataValue) {
   const normalizedValue = (dataValue - minDataValue) / (maxDataValue - minDataValue);
   return Math.round(minPitch + normalizedValue * pitchRange);
 }
+
 function startMusic(audioContext) {
   let index = 0;
   const interval = 2000; // Simulated data update interval (2 seconds)
@@ -45,8 +46,8 @@ function startMusic(audioContext) {
     // Use the data to control synth parameters (e.g., pitch and volume)
     const dataValue = parseFloat(csvData[index]['Height (m)']);
     const pitch = mapDataToPitch(dataValue);
-    //const volume = mapDataToVolume(dataValue);
     const volume = 0.5;
+
     // Calculate the duration of the previous note (in milliseconds)
     const previousNoteDuration = synth.getEnvelopeAtTime(audioContext.currentTime).release;
 
@@ -54,7 +55,7 @@ function startMusic(audioContext) {
     const delay = previousNoteDuration * 1000;
 
     // Schedule the next note with a delay
-    synth.triggerAttackRelease(pitch, '1n', 2.11, 4.5, volume, audioContext.currentTime + delay / 1000);
+    synth.triggerAttackRelease(pitch, '1n', undefined, volume, audioContext.currentTime + delay / 1000);
 
     // Move to the next data point
     index = (index + 1) % csvData.length;
@@ -88,7 +89,16 @@ document.getElementById('startButton').addEventListener('click', () => {
   startMusic(audioContext);
 });
 
-
+// Add event listener for the 'stopButton'
+document.getElementById('stopButton').addEventListener('click', () => {
+  // Check if the synth object exists and is valid
+  if (synth) {
+    // Release all notes
+    synth.triggerRelease();
+    synth.dispose(); // Dispose of the synth to disconnect it from the audio context
+    synth = undefined; // Set synth to undefined to allow reinitialization
+  }
+});
 
 // Fetch CSV data from the URL
 fetch(csvURL)
@@ -98,7 +108,7 @@ fetch(csvURL)
     csvData = parseCSV(data);
 
     // Calculate min and max values dynamically from the CSV data
-    const heights = csvData.map((row) => parseFloat(row['Height (m)']));
+    const heights = csvData.map((row) => parseFloat(row['Height (m)'));
     minDataValue = Math.min(...heights);
     maxDataValue = Math.max(...heights);
   })
@@ -108,42 +118,7 @@ fetch(csvURL)
 
 // Create audio components and event listeners within the DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', function() {
-  // Add event listener for the 'startButton'
-  const startButton = document.getElementById('startButton');
-  if (startButton) {
-    startButton.addEventListener('click', () => {
-      // Create an AudioContext within a user gesture (e.g., button click)
-      const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-
-      // Initialize audio components within the user gesture event
-      synth = new Tone.Synth().toDestination();
-      reverb = new Tone.Reverb({
-        decay: 60,
-       // wet: 1,
-      });
-
-      const reverbGain = new Tone.Gain().connect(reverb);
-      reverbGain.toDestination();
-
-      // Connect the synth to the reverb through the gain node
-      synth.connect(reverbGain);
-
-      // Start playing when the user clicks the "Start Music" button
-      startMusic();
-    });
-  }
-
-  // Add event listener for the 'stopButton'
-  const stopButton = document.getElementById('stopButton');
-  if (stopButton) {
-    stopButton.addEventListener('click', () => {
-      // Check if the synth object exists and is valid
-      if (synth) {
-        // Release all notes
-        synth.triggerRelease();
-        synth.dispose(); // Dispose of the synth to disconnect it from the audio context
-        synth = undefined; // Set synth to undefined to allow reinitialization
-      }
-    });
-  }
+  // You can add any additional setup or event listeners here if needed.
 });
+
+
